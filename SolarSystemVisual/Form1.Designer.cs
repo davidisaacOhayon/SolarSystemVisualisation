ee6db93f82a9cf6c546fa9d47f17add4c721fa19
David Isaac Ohayon <david.isaac.oh@gmail.com>
2024-07-04 10:48:07 +0200

    Updated Form Design

--- SolarSystemVisual/Form1.Designer.cs
+++ SolarSystemVisual/Form1.Designer.cs
@@ -29,14 +29,57 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.PlanetPropertyBox = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.PlanetPropertyBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.PlanetPropertyBox);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(2, 5);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1272, 537);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 504);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            this.label1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.label1_PreviewKeyDown);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.White;
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Sun",
+            "Planet"});
+            this.checkedListBox1.Location = new System.Drawing.Point(19, 19);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(179, 129);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // PlanetPropertyBox
+            // 
+            this.PlanetPropertyBox.Controls.Add(this.checkedListBox1);
+            this.PlanetPropertyBox.Location = new System.Drawing.Point(10, 7);
+            this.PlanetPropertyBox.Name = "PlanetPropertyBox";
+            this.PlanetPropertyBox.Size = new System.Drawing.Size(438, 276);
+            this.PlanetPropertyBox.TabIndex = 2;
+            this.PlanetPropertyBox.TabStop = false;
+            this.PlanetPropertyBox.Text = "Planet Properties";
             // 
             // Form1
             // 
@@ -46,6 +89,10 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.PlanetPropertyBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -53,6 +100,9 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.GroupBox PlanetPropertyBox;
     }
 }
 
